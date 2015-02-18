19b61f4fbe8a6d060a8b14836f7ffcebb4bb073d
mngibso <mark@gibsonsoftware.com>
2015-02-17 22:07:41 -0700

    Initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/managebox-dev'
+  },
+
+  seedDB: true
+};
