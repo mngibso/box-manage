db705e15c16a0d2f65f2bdaae4aeca9121d33752
mngibso <mark@gibsonsoftware.com>
2015-03-16 15:09:44 -0600

    mongoURI undefined

--- server/app.js
+++ server/app.js
@@ -14,9 +14,9 @@ var config = require('./config/environment');
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options, function (err, res) {
   if (err) {
-    console.log ('ERROR connecting to: ' + mongoURI + '. ' + err);
+    console.log ('ERROR connecting to: ' + config.mongo.uri + '. ' + err);
   } else {
-    console.log ('Succeeded connected to: ' + mongoURI);
+    console.log ('Succeeded connected to: ' + config.mongo.uri);
   }
 });
 
